7223fc8a4cbfb38e469ea142c47612c7e46cb76e
will76 <williamlisboa8086@gmail.com>
2023-06-18 19:45:03 -0300

    fix: formatacao das habilidades

--- src/Habilidade.java
+++ src/Habilidade.java
@@ -3,8 +3,8 @@ import java.util.List;
 public class Habilidade {
     private List<String> habilidades;
 
-    public List<String> getHabilidades() {
-        return habilidades;
+    public String getHabilidades() {
+        return String.join(", ", habilidades);
     }
 
     public void setHabilidades(List<String> habilidades) {
